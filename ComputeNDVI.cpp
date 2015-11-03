5d2482c76ebb586ebe67e8f27d9aba85322353ee
zlazow <zlazow@Zacharys-MacBook-Pro.local>
2015-11-03 13:21:27 -0500

    GeoC
    Satellite Image Analysis

---
+++ ComputeNDVI.cpp
@@ -0,0 +1,101 @@
+#include "gdal_priv.h"
+#include "cpl_conv.h" // for CPLMalloc()
+#include "cpl_string.h"
+
+int main() {
+	//Open File
+	GDALDataset  *poDataset;
+    GDALAllRegister();
+    poDataset = (GDALDataset *) GDALOpen( "GE01.tif", GA_ReadOnly );
+
+    //Create new DataSet/File
+
+
+    const char *pszFormat = "GTiff";
+    GDALDriver *poDriver;
+    char **papszMetadata;
+    poDriver = GetGDALDriverManager()->GetDriverByName(pszFormat);
+
+    if( poDriver == NULL )
+        exit( 1 );
+
+
+	GDALDataset *poNDVI_Dataset;
+    papszMetadata = poDriver->GetMetadata();
+    if( CSLFetchBoolean( papszMetadata, GDAL_DCAP_CREATECOPY, FALSE ) ){
+        printf( "Driver %s supports CreateCopy() method.\n", pszFormat );
+
+
+		poNDVI_Dataset = poDriver->CreateCopy( "NDVI_GEO1.tif", poDataset, FALSE, 
+		                                NULL, NULL, NULL );
+	}
+
+	GDALRasterBand  *poBand_ndvi;
+	poBand_ndvi = poNDVI_Dataset->GetRasterBand(1);
+	//poBand_ndvi->Fill(0);
+
+
+	//Fetch Raster Band
+	GDALRasterBand  *poBand_r;
+	GDALRasterBand  *poBand_i;
+
+
+	int             nBlockXSize, nBlockYSize;
+	poBand_r = poDataset->GetRasterBand( 3 ); //3 is red
+	poBand_i = poDataset->GetRasterBand( 4 ); //4 is NIR
+	poBand_r->GetBlockSize( &nBlockXSize, &nBlockYSize );
+	poBand_i->GetBlockSize( &nBlockXSize, &nBlockYSize );
+	poBand_ndvi->GetBlockSize( &nBlockXSize, &nBlockYSize);
+	int nBlocksX = (poBand_r->GetXSize() + nBlockXSize-1)/ nBlockXSize;
+	int nBlocksY = (poBand_r->GetYSize() + nBlockYSize-1)/ nBlockYSize;
+
+	//do I ever free these?
+	GByte *pRedDataBuf = (GByte *) CPLMalloc(nBlockXSize * nBlockYSize);
+	GByte *pNIRDataBuf = (GByte *) CPLMalloc(nBlockXSize * nBlockYSize);
+	GByte *pNDVIBuf    = (GByte *) CPLMalloc(nBlockXSize * nBlockYSize);
+
+	for (int iBlockY = 0; iBlockY < nBlocksY ; iBlockY++){
+		for (int iBlockX = 0; iBlockX <nBlocksX; iBlockX++){
+			float red, nir, ndvi;
+			int nValidX, nValidY;
+			poBand_r->ReadBlock(iBlockX, iBlockY, pRedDataBuf);
+			poBand_i->ReadBlock(iBlockX, iBlockY, pNIRDataBuf);
+			
+			 if( (iBlockX+1) * nBlockXSize > poBand_r->GetXSize() )
+                 nValidX = poBand_r->GetXSize() - iBlockX * nBlockXSize;
+             else
+                 nValidX = nBlockXSize;
+             if( (iBlockY+1) * nBlockYSize > poBand_r->GetYSize() )
+                 nValidY = poBand_r->GetYSize() - iBlockY * nBlockYSize;
+             else
+                 nValidY = nBlockYSize;
+			
+            for( int iY = 0; iY < nValidY; iY++ ){
+                for( int iX = 0; iX < nValidX; iX++ ){
+                    red = (float)pRedDataBuf[iX + iY * nBlockXSize];
+					nir = (float)pNIRDataBuf[iX + iY * nBlockXSize];
+					ndvi = (nir-red)/(nir+red);
+					ndvi = 127.5 + ndvi * 127.5;
+					//printf("%f, %f, %f ... ", red, nir, ndvi);
+					pNDVIBuf[iX + iY * nBlockXSize] = (uint8_t)ndvi;
+                }
+            }
+             //printf("Got here1 %i, %i \n", iBlockX, iBlockY);
+             poBand_ndvi->WriteBlock(iBlockX, iBlockY, pNDVIBuf);
+             //printf("Got here2\n");
+             
+		}
+
+
+	}
+	printf("error is in closing or writeout\n");
+
+	//save/write image file to storage
+	//NDVI = (NIR-red)/(NIR+red)
+
+	//Close Dataset
+	GDALClose(poDataset);
+	GDALClose( (GDALDatasetH) poNDVI_Dataset );
+	//Exit      
+	return 0;
+}
---
+++ metadata.cpp
@@ -0,0 +1,140 @@
+#include "gdal_priv.h"
+#include "cpl_conv.h" // for CPLMalloc()
+
+///Library/Frameworks/GDAL.framework/Versions/1.11/Headers/
+///Library/Frameworks?/GDAL.framework/unix/bin
+
+//g++ -I/Library/Frameworks/GDAL.framework/Versions/1.11/Headers -L/Library/Frameworks/GDAL.framework/unix/lib -lgdal metadata.cpp
+/* 
+result: main.o
+	g++ main.o -L/ -lgdal -o result
+main.o: main.cpp main.h
+	g++ -c main.cpp -I/ -o main.o
+
+     poBand->GetBlockSize( &nXBlockSize, &nYBlockSize );
+     int nXBlocks = (poBand->GetXSize() + nXBlockSize - 1) / nXBlockSize;
+     int nYBlocks = (poBand->GetYSize() + nYBlockSize - 1) / nYBlockSize;
+     GByte *pabyData = (GByte *) CPLMalloc(nXBlockSize * nYBlockSize);
+     for( int iYBlock = 0; iYBlock < nYBlocks; iYBlock++ )
+     {
+         for( int iXBlock = 0; iXBlock < nXBlocks; iXBlock++ )
+         {
+             int        nXValid, nYValid;
+             poBand->ReadBlock( iXBlock, iYBlock, pabyData );
+             // Compute the portion of the block that is valid
+             // for partial edge blocks.
+             if( (iXBlock+1) * nXBlockSize > poBand->GetXSize() )
+                 nXValid = poBand->GetXSize() - iXBlock * nXBlockSize;
+             else
+                 nXValid = nXBlockSize;
+             if( (iYBlock+1) * nYBlockSize > poBand->GetYSize() )
+                 nYValid = poBand->GetYSize() - iYBlock * nYBlockSize;
+             else
+                 nYValid = nYBlockSize;
+             // Collect the histogram counts.
+             for( int iY = 0; iY < nYValid; iY++ )
+             {
+                 for( int iX = 0; iX < nXValid; iX++ )
+                 {
+                     panHistogram[pabyData[iX + iY * nXBlockSize]] += 1;
+                 }
+             }
+         }
+     }
+ }
+
+CPLErr GetHistogram( GDALRasterBand *poBand, GUIntBig *panHistogram )
+ {
+     memset( panHistogram, 0, sizeof(GUIntBig) * 256 );
+     CPLAssert( poBand->GetRasterDataType() == GDT_Byte );
+     int nXBlockSize, nYBlockSize;
+     poBand->GetBlockSize( &nXBlockSize, &nYBlockSize );
+     int nXBlocks = (poBand->GetXSize() + nXBlockSize - 1) / nXBlockSize;
+     int nYBlocks = (poBand->GetYSize() + nYBlockSize - 1) / nYBlockSize;
+     GByte *pabyData = (GByte *) CPLMalloc(nXBlockSize * nYBlockSize);
+     for( int iYBlock = 0; iYBlock < nYBlocks; iYBlock++ )
+     {
+         for( int iXBlock = 0; iXBlock < nXBlocks; iXBlock++ )
+         {
+             int        nXValid, nYValid;
+             poBand->ReadBlock( iXBlock, iYBlock, pabyData );
+             // Compute the portion of the block that is valid
+             // for partial edge blocks.
+             if( (iXBlock+1) * nXBlockSize > poBand->GetXSize() )
+                 nXValid = poBand->GetXSize() - iXBlock * nXBlockSize;
+             else
+                 nXValid = nXBlockSize;
+             if( (iYBlock+1) * nYBlockSize > poBand->GetYSize() )
+                 nYValid = poBand->GetYSize() - iYBlock * nYBlockSize;
+             else
+                 nYValid = nYBlockSize;
+             // Collect the histogram counts.
+             for( int iY = 0; iY < nYValid; iY++ )
+             {
+                 for( int iX = 0; iX < nXValid; iX++ )
+                 {
+                     panHistogram[pabyData[iX + iY * nXBlockSize]] += 1;
+                 }
+             }
+         }
+     }
+ }
+*/
+
+
+int main()
+{
+    GDALDataset  *poDataset;
+    GDALAllRegister();
+    poDataset = (GDALDataset *) GDALOpen( "GE01.tif", GA_ReadOnly );
+    printf("Working! \n");
+
+    if( poDataset != NULL ){
+    	//Get Dataset Information
+        double adfGeoTransform[6];
+
+		printf( "Driver: %s/%s\n", poDataset->GetDriver()->GetDescription(), poDataset->GetDriver()->GetMetadataItem( GDAL_DMD_LONGNAME ) );
+
+		printf( "Size is %dx%dx%d\n", poDataset->GetRasterXSize(), poDataset->GetRasterYSize(), poDataset->GetRasterCount() );
+
+		if( poDataset->GetProjectionRef()  != NULL )
+    		printf( "Projection is `%s'\n", poDataset->GetProjectionRef() );
+
+		if( poDataset->GetGeoTransform( adfGeoTransform ) == CE_None ){
+    		printf( "Origin = (%.6f,%.6f)\n",
+            adfGeoTransform[0], adfGeoTransform[3] );
+    		printf( "Pixel Size = (%.6f,%.6f)\n",
+            adfGeoTransform[1], adfGeoTransform[5] );
+		}
+
+		//Fetch Raster Band
+		GDALRasterBand  *poBand;
+		int             nBlockXSize, nBlockYSize;
+		int             bGotMin, bGotMax;
+		double          adfMinMax[2];
+		poBand = poDataset->GetRasterBand( 1 );
+		poBand->GetBlockSize( &nBlockXSize, &nBlockYSize );
+		printf( "Block=%dx%d Type=%s, ColorInterp=%s\n",
+		        nBlockXSize, nBlockYSize,
+		        GDALGetDataTypeName(poBand->GetRasterDataType()),
+		        GDALGetColorInterpretationName(
+		            poBand->GetColorInterpretation()) );
+		adfMinMax[0] = poBand->GetMinimum( &bGotMin );
+		adfMinMax[1] = poBand->GetMaximum( &bGotMax );
+		if( ! (bGotMin && bGotMax) )
+		    GDALComputeRasterMinMax((GDALRasterBandH)poBand, TRUE, adfMinMax);
+		printf( "Min=%.3fd, Max=%.3f\n", adfMinMax[0], adfMinMax[1] );
+		if( poBand->GetOverviewCount() > 0 )
+		    printf( "Band has %d overviews.\n", poBand->GetOverviewCount() );
+		if( poBand->GetColorTable() != NULL )
+		    printf( "Band has a color table with %d entries.\n", 
+		             poBand->GetColorTable()->GetColorEntryCount() );
+
+		//Close Dataset
+		GDALClose(poDataset);
+		//Exit      
+		return 0;
+	}
+
+
+}
